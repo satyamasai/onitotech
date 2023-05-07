dfd01da188c29017c8b0c12e689ef97facb7f78e
satyam gupta <stymngrya@gmail.com>
2023-05-07 16:23:31 +0530

    form alignment

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,93 +1,111 @@
 import React from "react";
+// import { Button } from '@mui/material/Button';
 
 const Home = () => {
+  const hadnleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.sex.value);
+  };
+
   return (
     <div className="home">
-      <form>
+      <form onSubmit={hadnleSubmit}>
         {/* personal details section */}
 
         <div className="personal_details">
           <div className="heading">
-            <h3>Personal Details</h3>
+            <h3>Personal Details : </h3>
           </div>
           <div className="personals_input">
             <div className="name_mobile_div">
               <div>
-                <label htmlFor="">Name*</label>
+                <label htmlFor="">Name <span>*</span></label>
                 <input type="text" name="name" placeholder="Name" />
               </div>
 
               <div>
-                <label htmlFor="">Mobile*</label>
+                <label htmlFor="">Mobile<span>*</span> </label>
                 <input type="text" name="mobile" placeholder="Mobile" />
               </div>
             </div>
             <div className="age_sex_gov_div">
               <div className="age_sex_div">
-                <div>
-                  <label htmlFor="">Age*</label>
+                <div className="age_div">
+                  <label htmlFor="">Age<span>*</span> </label>
                   <input type="text" name="age" placeholder="Age" />
                 </div>
-                <div>
-                  <label htmlFor="">Sex*</label>
-                  <select>
+                <div className="sex_div">
+                  <label htmlFor="">Sex<span>*</span></label>
+                  <select name="sex">
                     <option value="">Sex</option>
-                    <option value="">M</option>
-                    <option value="">F</option>
+                    <option value="male">M</option>
+                    <option value="female">F</option>
                   </select>
                 </div>
               </div>
               <div className="govt_id_div">
-                <label htmlFor="">Govt issued ID*</label>
+                <label htmlFor="">Govt issued ID<span>*</span></label>
 
                 <div>
-                  <select>
+                  <select name="id_type">
                     <option value="">ID type</option>
                     <option value="">Aadhar</option>
                     <option value="">PAN</option>
                   </select>
-                  <input type="text" placeholder="govt id" />
+                  <input
+                    name="govt_id_number"
+                    type="text"
+                    placeholder="govt id Number"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* contact details section */}
+        {/*-------------------- contact details section ----------------*/}
 
         <div className="contact_details">
           <div className="heading">
-            <h3>Contact Details</h3>
+            <h3>Contact Details : </h3>
           </div>
           <div className="contact_input">
             <div className="gaurdian_details">
-              <label htmlFor="">Guardian Details</label>
-              <select name="" id="">
+              <label htmlFor="">Guardian Details </label>
+              <select name="guardian" id="">
                 <option value="">Select Guardian</option>
                 <option value="">Mother</option>
                 <option value="">Father</option>
               </select>
 
-              <input type="text" placeholder="Enter Guardian Name" />
+              <input
+                type="text"
+                name="guardian_name"
+                placeholder="Enter Guardian Name"
+              />
             </div>
 
             <div className="email_emergency">
               <div className="email">
-                <label htmlFor="">Email *</label>
-                <input type="text" placeholder="Enter Email" />
+                <label htmlFor="">Email </label>
+                <input type="text" name="email" placeholder="Enter Email" />
               </div>
 
               <div className="emergency">
-                <label htmlFor="">Emergency number *</label>
-                <input type="text" placeholder="Enter Emergency number" />
+                <label htmlFor="">Emergency number </label>
+                <input
+                  type="text"
+                  name="emergency_num"
+                  placeholder="Enter Emergency number"
+                />
               </div>
             </div>
           </div>
         </div>
-        {/* contact details section */}
+        {/* ----------contact details section-------------- */}
 
         <div className="address_details">
           <div className="heading">
-            <h3>Address Details</h3>
+            <h3>Address Details : </h3>
           </div>
           <div className="address_inputs">
             <div className="address_city">
@@ -112,28 +130,28 @@ const Home = () => {
             </div>
             <div className="state_pin_country">
               <div className="state_pin">
-              <div>
-              <label htmlFor="state">State: </label>
-              <select type="text" id="state" name="state">
-                  <option value="">Select State</option>
-                  <option value="">Delhi</option>
-                  <option value="">Maharastra</option>
-                  <option value="">Madhya Pradesh</option>
-                  <option value="">Uttar Pradesh</option>
-                  <option value="">Haryana</option>
-                  <option value="">Punjab</option>
-                </select>
+                <div>
+                  <label htmlFor="state">State: </label>
+                  <select type="text" id="state" name="state">
+                    <option value="">Select State</option>
+                    <option value="">Delhi</option>
+                    <option value="">Maharastra</option>
+                    <option value="">Madhya Pradesh</option>
+                    <option value="">Uttar Pradesh</option>
+                    <option value="">Haryana</option>
+                    <option value="">Punjab</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="pin">Pin: </label>
+                  <input
+                    placeholder="Enter PIN"
+                    type="text"
+                    id="pin"
+                    name="pin"
+                  />
+                </div>
               </div>
-<div>
-<label htmlFor="pin">Pin: </label>
-<input
-placeholder="Enter PIN"
-type="text"
-id="pin"
-name="pin"
-/>
-</div>
-</div>
               <div className="country">
                 <label htmlFor="country">Country:</label>
                 <input
@@ -149,25 +167,28 @@ name="pin"
         {/*--------------- others details section -----------*/}
         <div className="others_details">
           <div className="heading">
-            <h3>Others Details</h3>
+            <h3>Others Details : </h3>
           </div>
           <div className="others_input">
             <div className="occu_nationality">
               <div className="occupation">
                 <label htmlFor="">Occupation </label>
 
-                <input type="text" placeholder="Occupation" />
+                <input name="occupation" type="text" placeholder="Occupation" />
               </div>
               <div className="nationality">
-              <label htmlFor="">Nationality </label>
-              <input type="text" name="nationality" placeholder="Nationality" />
-            </div>
-
+                <label htmlFor="">Nationality </label>
+                <input
+                  type="text"
+                  name="nationality"
+                  placeholder="Nationality"
+                />
+              </div>
             </div>
 
             <div className="Religion">
               <label htmlFor="">Religion </label>
-              <select type="text">
+              <select type="text" name="religion">
                 <option value="">Select Religion</option>
                 <option value="">Hindu</option>
                 <option value="">Muslim</option>
@@ -188,7 +209,7 @@ name="pin"
             </div>
             <div className="blood_grp">
               <label htmlFor="">Blood Group</label>
-              <select name="" id="">
+              <select name="blood_group" id="">
                 <option value="">Blood Group</option>
                 <option value="">A+</option>
                 <option value="">A-</option>
@@ -200,8 +221,12 @@ name="pin"
                 <option value="">O-</option>
               </select>
             </div>
-
-         
+          </div>
+        </div>
+        <div className="submit_div">
+          <div>
+            <button className="cancel_btn">Cancel</button>
+            <input className="submit_btn" type="submit" value={"Submit"} />
           </div>
         </div>
       </form>
